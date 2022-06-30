32a18f711dfb9254009292e91e17df294821751a
alphanse <alphanse98@gmail.com>
2022-06-30 14:29:37 +0530

    files

---
+++ files/app.js
@@ -0,0 +1,56 @@
+a = parseInt(prompt("a ="));
+b = parseInt(prompt("b ="));
+c = parseInt(prompt("c ="));
+
+console.log('Ascending' )
+if ( a < b)
+  if ( a < c){
+      if (b < c){
+          console.log(a , b, c);
+      }
+      else{
+          console.log( a, c, b);
+      }
+    }
+    else{
+        console.log(c, a, b);
+    }
+
+else if (b < c) {
+    if(a < c){
+        console.log(b, a, c);
+    }
+    else{
+        console.log(b, c, a);
+    }
+}
+    else{
+        console.log(c , b, a);
+    }
+
+    console.log('Descending' )
+
+    if ( a > b)
+  if ( a > c){
+      if (b > c){
+          console.log(a , b, c);
+      }
+      else{
+          console.log( a, c, b);
+      }
+    }
+    else{
+        console.log(c, a, b);
+    }
+
+else if (b > c) {
+    if(a > c){
+        console.log(b, a, c);
+    }
+    else{
+        console.log(b, c, a);
+    }
+}
+    else{
+        console.log(c , b, a);
+    }
